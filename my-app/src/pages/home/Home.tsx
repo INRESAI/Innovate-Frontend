a888ed23caafaabadec246725ccbc1d2c68518fb
bakachan <kienn11000@gmail.com>
2023-01-12 16:28:53 +0700

    update_header_v2

--- my-app/src/pages/home/Home.tsx
+++ my-app/src/pages/home/Home.tsx
@@ -2,33 +2,33 @@ import { Button } from 'antd'
 import React from 'react'
 import './styles.home.scss'
 import ImageOfIntro from '../../images/intro_image.png'
-import HowToUse from  '../../images/how_to_use.png'
-import HowToUse1 from  '../../images/how_to_use1.png'
-import HowToUse2 from  '../../images/how_to_use2.png'
-import HowToUse3 from  '../../images/how_to_use3.png'
-import HowToUse4 from  '../../images/how_to_use4.png'
-import HowToUse5 from  '../../images/how_to_use5.png'
+import HowToUse from '../../images/how_to_use.png'
+import HowToUse1 from '../../images/how_to_use1.png'
+import HowToUse2 from '../../images/how_to_use2.png'
+import HowToUse3 from '../../images/how_to_use3.png'
+import HowToUse4 from '../../images/how_to_use4.png'
+import HowToUse5 from '../../images/how_to_use5.png'
 import IntroMap from '../../images/intro-map.png'
 
 
 const Home = () => {
     return (
-        <div>
+        <div className='main-home'>
             <div className='intro-part'>
                 <div className='text-of-intro'>
                     <div className='title' >Tổ chức Giáo dục Đại học của bạn đã chuẩn bị sẵn sàng cho những thách thức trong tương lai chưa?</div>
                     <div className='detail'>Quốc gia khởi nghiệp là một trong những ưu tiên phát triển của Việt Nam từ năm 2016 đến nay. Nhiều chương trình quốc gia đã được phê duyệt và triển khai sâu rộng trên toàn quốc như đề án 844, 1665, 939. Trường đại học, cao đẳng cần là cốt lõi cho hệ sinh thái khởi nghiệp, đổi mới sáng tạo quốc gia, vì ở đó là nơi tạo ra tri thức, chia sẻ tri thức, cũng như phát triển nhân tài cho đổi mới sáng tạo và khởi nghiệp.</div>
                     <div>
-                        <Button>Bắt đầu tự đánh giá</Button>
-                        <img src=''/>
+                        <Button className='button-start'>Bắt đầu tự đánh giá</Button>
+                        <img src='' />
                     </div>
                 </div>
                 <div className='image-of-intro'>
-                    <img src={ImageOfIntro}/>
+                    <img src={ImageOfIntro} />
                 </div>
             </div>
             <div className='right-of-user'>
-                <div style={{    height: "225px"}}></div>
+                <div style={{ height: "225px" }}></div>
                 <div className='title'>
                     Quyền lợi khi sử dụng U.innovate
                 </div>
@@ -58,30 +58,30 @@ const Home = () => {
             </div>
             <div className='how-to-use'>
                 <div className='tutorial-image'>
-                    <img src={HowToUse}/>
+                    <img src={HowToUse} />
                 </div>
                 <div className='content'>
                     <div className='title'>Làm thế nào để sử dụng U.innovate?</div>
                     <div className='subtitle'>Các bước dưới đây hướng dẫn bạn về các tính năng và nội dung của U.innovate.</div>
                     <div className='step-lst'>
                         <div className='lst-item'>
-                            <img className='img-item' src={HowToUse1}/>
+                            <img className='img-item' src={HowToUse1} />
                             <div>Bạn cần tạo tài khoản của mình trên U.innovate</div>
                         </div>
                         <div className='lst-item'>
-                            <img className='img-item' src={HowToUse2}/>
+                            <img className='img-item' src={HowToUse2} />
                             <div>Bạn có thể có cái nhìn tổng quan về các tiêu chí và phương pháp của U.innovate</div>
                         </div>
-                        <div className='lst-item'>  
-                            <img className='img-item' src={HowToUse3}/>
+                        <div className='lst-item'>
+                            <img className='img-item' src={HowToUse3} />
                             <div>Bạn tiến hành tự động đánh giá thông qua các bước trên nền tảng U.innovate</div>
                         </div>
                         <div className='lst-item'>
-                            <img className='img-item' src={HowToUse4}/>
+                            <img className='img-item' src={HowToUse4} />
                             <div>Bạn có thể đọc báo cáo về cơ sở giáo dục của mình trên U.innovate hoặc download báo cáo về máy, hoặc gửi email báo cáo</div>
                         </div>
                         <div className='lst-item'>
-                            <img className='img-item' src={HowToUse5}/>
+                            <img className='img-item' src={HowToUse5} />
                             <div>Bạn có thể quay lại để tham khảo sự thay đổi của kết quả tự đánh giá và kết quả so sánh sau một thời gian</div>
                         </div>
                     </div>
@@ -91,7 +91,7 @@ const Home = () => {
                 <img src={IntroMap}></img>
             </div>
             <div className='our-partner'>
-                <div style={{height: "43px"}}></div>
+                <div style={{ height: "43px" }}></div>
                 <div className='title'>Đối tác của chúng tôi</div>
                 <div className='partner-lst'>
 
