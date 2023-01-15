546aa4111b842be151fa49d54331877cbd06b5c8
Trung Hieu Do <hieu.yl.ptit1312@gmail.com>
2023-01-15 21:31:31 +0700

    add little css and call API comment

--- my-app/src/pages/Judgement/JudgementMain.tsx
+++ my-app/src/pages/Judgement/JudgementMain.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import IntroduceMethod from './IntroduceMethod'
 import JudgementCriteriaOptions from './JudgementCriteriaOptions'
 import TakingTest from './TakingTest'
@@ -8,13 +8,20 @@ const JudgementMain = () => {
     const [isShowIntro,setIsShowIntro] = useState(true);
     const [isShowCriteria,setIsShowCriteria] = useState(false);
     const [isShowTest,setIsShowTest] = useState(false);
+    //Dung useSelector lay ra 2 lst criteriaLst va questionByCriteriaLst
+
+    useEffect(()=>{
+        //call API get All criteria va luu vao Redux
+    },[])
 
     const tranferFromIntroToCriteria = () => {
         setIsShowIntro(false);
         setIsShowCriteria(true);
     }
 
-    const tranferFromCriteriaToTest = () => {
+    const tranferFromCriteriaToTest = (criteriaId: string) => { // chuyen tu man chon tieu chi sang man lam bai test
+        //Call API get danh sach question theo criteriaId
+
         setIsShowCriteria(false);
         setIsShowTest(true);
     }
@@ -43,13 +50,16 @@ const JudgementMain = () => {
                 <JudgementCriteriaOptions
                     tranferFromCriteriaToTest = {tranferFromCriteriaToTest}
                     revertToIntro = {revertToIntro}
+                    //Sau nay se truyen them 1 lst Criteria vao day
                 />
             }
             {
+                // Sau nay se sua isShowTest thanh questionByCriteria != null
                 isShowTest && 
                 <TakingTest
                     revertToIntro = {revertToIntro}
                     revertToCriteria = {revertToCriteria}
+                    //Sau nay se truyen 1 lst questionByCriteriaLst vao nua
                 />
             }
         </div>
